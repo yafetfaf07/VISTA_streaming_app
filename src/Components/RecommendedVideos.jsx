f5361f6febb3ce696ce35ffe3914a20f19f94b22
Yafet Yosef <japhetkumalo23@gmail.com>
2024-06-03 16:42:50 +0300

    Added the Recommended Components to the video detail page

---
+++ src/Components/RecommendedVideos.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import pics from "../assets/images/backgroundImage.webp"
+import "../CSS_files/video-details.css"
+
+const RecommendedVideos = () => {
+  return (
+    <div style={{display:"flex", alignItems:"center"}}>
+
+     <div className='recommended-videos-thummbnail-wrapper'>
+        <img src={pics} alt='' />
+        <div className='recommended-videos-time-wrapper'>
+            <span>25:00 </span>
+        </div>
+     </div>
+
+     <div className='recommended-videos-details'>
+        <div>
+            <div style={{display:"flex", flexDirection:"column"}}>
+            <span className='recommended-video-title'>Halo 2 is a bust!!</span>
+
+            </div>
+        </div>
+        <div>
+        <p className='recommended-channel-name'> Buno Reacts </p>
+        </div>
+        <div>
+            <span className='recommended-channel-name'>5K Views</span>
+            <p className='recommended-channel-name'>3 days ago</p>
+
+        </div>
+     </div>
+    </div>
+  )
+}
+
+export default RecommendedVideos
