92dc97f0e8fa20a11f7d2063024e4362531aa787
Yafet Yosef <japhetkumalo23@gmail.com>
2024-06-04 19:58:31 +0300

    Added the video upload icon

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,32 +1,34 @@
-import React from 'react'
-import LoginButton from '../Components/LoginButton'
-import LogoutButton from '../Components/LogoutButton'
-import Profile from '../Components/Profile'
-import logo from "../assets/images/Logo-removebg-preview.png"
-import "../CSS_files/movie-main.css"
-
+import React from "react";
+import LoginButton from "../Components/LoginButton";
+import LogoutButton from "../Components/LogoutButton";
+import Profile from "../Components/Profile";
+import logo from "../assets/images/Logo-removebg-preview.png";
+import "../assets/CSS_files/movie-main.css";
+import { useNavigate } from "react-router-dom";
 
 const NavBar = () => {
+  const navigate = useNavigate();
+
   return (
     <nav className="nav-bar">
-    <div className='logos'>
-<img src={logo} alt='' />
-<h2>Mumble</h2>
-    </div>
-    <div className='search'>
-        <input type='text' placeholder='Search movies' />
-    </div>
-    {
-   
-}
-    <div className='interaction'>
-       <LoginButton />
-       <Profile />
-       <LogoutButton />  
+      <div className="logos">
+        <img src={logo} alt="" />
+        <h2>Mumble</h2>
+      </div>
+      <div className="search">
+        <input type="text" placeholder="Search movies" />
+      </div>
+      {}
+      <div className="interaction">
+        <span class="material-symbols-outlined" onClick={() => {
+          navigate('/videoupload');
+        }}>videocam</span>
+        <LoginButton />
+        <Profile />
+        <LogoutButton />
+      </div>
+    </nav>
+  );
+};
 
-    </div>
-</nav>
-  )
-}
-
-export default NavBar
+export default NavBar;
