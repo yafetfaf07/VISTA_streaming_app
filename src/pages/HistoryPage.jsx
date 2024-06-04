44d1e24783619a4b8aade98f53c0fd3127e710e4
Yafet Yosef <japhetkumalo23@gmail.com>
2024-06-04 09:51:28 +0300

    Added the History page

---
+++ src/pages/HistoryPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../assets/CSS_files/history.css"
+const HistoryPage = () => {
+  return (
+<div className='history-page'>
+<h2>Watch History</h2>
+
+</div>
+    )
+}
+
+export default HistoryPage
