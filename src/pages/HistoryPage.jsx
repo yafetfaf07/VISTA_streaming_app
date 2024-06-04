63480e771d0b37a3c9c69b55267d64df2d7d2a27
Yafet Yosef <japhetkumalo23@gmail.com>
2024-06-04 19:55:36 +0300

    Updated the History page

--- src/pages/HistoryPage.jsx
+++ src/pages/HistoryPage.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
-import "../assets/CSS_files/history.css"
+import React from "react";
+import "../assets/CSS_files/history.css";
+import NavBar from "../Components/NavBar";
+import Sidebar from "../Components/Sidebar";
+import HistoryCard from "../Components/HistoryCard";
 const HistoryPage = () => {
   return (
-<div className='history-page'>
-<h2>Watch History</h2>
+    <>
+    <NavBar />
+    <main style={{display:"flex"}}>
+    <Sidebar />
+      <div className="history-page">
+        <h2>Watch History</h2>
+        <div>
+        <HistoryCard />
+        <HistoryCard />
+        <HistoryCard />
+        <HistoryCard />
+        <HistoryCard />
+        <HistoryCard />
+        </div>
+     
 
-</div>
-    )
-}
+      </div>
+    </main>
+   
+    </>
+  );
+};
 
-export default HistoryPage
+export default HistoryPage;
