801e04273f85c30e98403041ff402b5a7d4e5db0
Yafet Yosef <japhetkumalo23@gmail.com>
2024-06-28 19:14:22 +0300

    Wrapped the Comment Card in the CommentSection component

--- src/Components/CommentSection.jsx
+++ src/Components/CommentSection.jsx
@@ -1,13 +1,34 @@
 import React, { useState } from "react";
 import "../assets/CSS_files/comment.css";
+import profileimage from "../assets/images/backgroundImage.webp";
+import CommentCard from "./CommentCard";
 
 const CommentSection = () => {
   const [totalComments, settotalComments] = useState(2);
   return (
-    <div className="comment-section-wrapper">
-      <h2>
-        <p className="total-comments">{totalComments}</p>Comment
-      </h2>
+    <div>
+      <div className="comment-section-wrapper">
+        <h2>
+          <p className="total-comments">{totalComments}</p>Comment
+        </h2>
+        <div className="add-comment">
+          <img src={profileimage} alt="" className="profile-image" />
+          <input type="text" placeholder="Add a comment.." />
+          <button>Post</button>
+        </div>
+      </div>
+      <div className="comment-card-wrapper">
+        <CommentCard />
+        <CommentCard />
+        <CommentCard />
+        <CommentCard />
+        <CommentCard />
+        <CommentCard />
+        <CommentCard />
+        <CommentCard />
+        <CommentCard />
+        <CommentCard />
+      </div>
     </div>
   );
 };
