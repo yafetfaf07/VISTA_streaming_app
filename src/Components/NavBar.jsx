3e79ec424de1bae9ed1b4114983446655e9da267
Yafet Yosef <japhetkumalo23@gmail.com>
2024-06-28 19:11:47 +0300

    Added the logic for not getting to the profile page without sign up or login in

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -5,14 +5,17 @@ import Profile from "../Components/Profile";
 import logo from "../assets/images/Logo-removebg-preview.png";
 import "../assets/CSS_files/movie-main.css";
 import { useNavigate } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const NavBar = () => {
   const navigate = useNavigate();
-
+  const {loginWithRedirect, isAuthenticated} = useAuth0()
   return (
     <nav className="nav-bar">
       <div className="logos">
-        <img src={logo} alt="" />
+        <img src={logo} alt="" onClick={ () => {
+          navigate('/');
+        }}/>
         <h2>Mumble</h2>
       </div>
       <div className="search">
@@ -21,8 +24,14 @@ const NavBar = () => {
       {}
       <div className="interaction">
         <span class="material-symbols-outlined" onClick={() => {
-          navigate('/videoupload');
-        }}>videocam</span>
+          if(!isAuthenticated) {
+            loginWithRedirect();
+          }
+          else {
+            navigate('/videoupload');
+
+          }
+        }}>person_add</span>
         <LoginButton />
         <Profile />
         <LogoutButton />
