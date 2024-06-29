e1176dfa5fd56421669937a1475d07bbfc7840b6
Yafet Yosef <japhetkumalo23@gmail.com>
2024-06-29 16:43:47 +0300

    Updated the Routing configurations

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import HomePage from './pages/HomePage';
-import SignInPage from './pages/SignInPage';
 import MoviePage from './pages/MoviePage';
 import UserContextProvider from './context/UserContextProvider';
-import VideoDetails from './Components/VideoDetails';
+import VideoDetailsPage from './pages/VideoDetailsPage';
+import HistoryPage from './pages/HistoryPage';
+import VideoUploadPage from './pages/VideoUploadPage';
+import ProfilePage from './pages/ProfilePage';
+
+import './App.css'
 
 const App = () => {
   return (
   <UserContextProvider>
 <Router>
 <Routes>
-  <Route path='/' element={<MoviePage/>}>
-  </Route>
-  <Route path='/videodetail' element={<VideoDetails />} />
+  <Route path='/' element={<MoviePage/>}></Route>
+  <Route path='/videodetail' element={<VideoDetailsPage />} />
+  <Route path='/history' element= {<HistoryPage />} />
+  <Route path='/videoupload' element = {<VideoUploadPage />}/>
+  <Route path='/profile' element = {<ProfilePage />}/>
 
 </Routes>
 </Router>
